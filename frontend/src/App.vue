ca9ac5daae8ed013a5afbf6bed5d84ad4b15ad54
Isaac Kalinkin <i.kalinkin@vk.team>
2024-02-19 20:28:16 +0300

    Add missing frontend files

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <div class="container mt-3">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+/* Global styles */
+</style>
